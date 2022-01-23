a9435efdcad3b8ec246e7e3ddc45d5fe9fcdc4c8
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-01-23 15:24:15 -0300

    add new page to use checkboxes

--- entrada_de_dados/lib/main.dart
+++ entrada_de_dados/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:entrada_de_dados/CampoTexto.dart';
 import 'package:flutter/material.dart';
 
+import 'EntradaCheckbox.dart';
+
 void main() {
   runApp(
     MaterialApp(
-      home: CampoTexto(),
+      home: EntradaCheckbox(),
     ),
   );
 }
