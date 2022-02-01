2bdc5c4087f6089c6e37c442d6ae04b9eca3eeaa
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-01-31 23:40:49 -0300

    using new http methods

--- consumo_servico_avancado/lib/Home2.dart
+++ consumo_servico_avancado/lib/Home2.dart
@@ -25,50 +25,130 @@ class _Home2State extends State<Home2> {
     return posts;
   }
 
+  void _post() async {
+    String corpo = json.encode(
+      {"userId": 120, "id": null, "title": "Titulo", "body": "corpo"},
+    );
+    http.Response response = await http.post(
+      Uri.parse(_urlBase + "/posts"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: corpo,
+    );
+    print(response.body);
+  }
+
+  void _put() async {
+    String corpo = json.encode(
+      {
+        "userId": 121,
+        "id": null,
+        "title": "Titulo alterado",
+        "body": "corpo alterado"
+      },
+    );
+    http.Response response = await http.put(
+      Uri.parse(_urlBase + "/posts/2"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: corpo,
+    );
+    print(response.body);
+  }
+
+  void _patch() async {
+    String corpo = json.encode(
+      {"userId": 121, "id": null, "title": null, "body": "corpo alterado"},
+    );
+    http.Response response = await http.patch(
+      Uri.parse(_urlBase + "/posts/2"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: corpo,
+    );
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  void _delete() async {
+    http.Response response = await http.delete(
+      Uri.parse(_urlBase + "/posts/2"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+    );
+    print(response.body);
+    print(response.statusCode);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Consumo de servico avançado"),
+        title: Text("Posts"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot) {
-          String resultado = "";
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              resultado = "Carregando";
-              break;
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                resultado = "Erro ao carregar os dados...";
-              } else {
-                // resultado = "Lista carregada";
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                RaisedButton(
+                  child: Text("POST"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("PUT"),
+                  onPressed: _put,
+                ),
+                RaisedButton(
+                  child: Text("PATCH"),
+                  onPressed: _patch,
+                ),
+                RaisedButton(
+                  child: Text("DELETE"),
+                  onPressed: _delete,
+                ),
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPostagens(),
+                builder: (context, snapshot) {
+                  String resultado = "";
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      resultado = "Carregando";
+                      break;
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      if (snapshot.hasError) {
+                        resultado = "Erro ao carregar os dados...";
+                      } else {
+                        // resultado = "Lista carregada";
 
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    List<Post> posts = snapshot.data!;
-                    Post post = posts[index];
+                        return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            List<Post> posts = snapshot.data!;
+                            Post post = posts[index];
 
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.body),
-                    );
-                  },
-                );
-              }
-              break;
-          }
-          return Center(
-            child: Text(resultado),
-          );
-        },
+                            return ListTile(
+                              title: Text(post.title),
+                              subtitle: Text(post.body),
+                            );
+                          },
+                        );
+                      }
+                      break;
+                  }
+                  return Center(
+                    child: Text(resultado),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
