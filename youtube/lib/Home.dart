ea875a15fdd3f9ae85c299ccd8e1d1a75df357d9
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-02-01 23:27:02 -0300

    add new botton navigation bar

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricao.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +12,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -36,7 +49,35 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice) {
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Início"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Em alta"),
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Inscrições"),
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Biblioteca"),
+            icon: Icon(Icons.folder),
+          ),
+        ],
+      ),
     );
   }
 }
