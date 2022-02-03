9490d2cf56da269707203047519afcf591d1f441
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-02-02 23:51:52 -0300

    adding request

--- youtube/lib/Api.dart
+++ youtube/lib/Api.dart
@@ -1,1 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const CHAVE_YOUTUBE_API = "AIzaSyAaEY1Rg0elo9nIbz4qsrUCgflfy3mC8Fc";
+const ID_CANAL = "edublinTV";
+const URL_BASE = "https://www.googleapis.com/youtube/v3";
+
+class Api {
+  pesquisar(String pesquisar) async {
+    http.Response response = await http.get(Uri.parse(
+      URL_BASE + "/search",
+    ));
+  }
+}
