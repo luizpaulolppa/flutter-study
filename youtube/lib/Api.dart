65b2c296cd6d1be19eee554d51e217a252b0e32e
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-02-05 23:45:02 -0300

    fix youtube request

--- youtube/lib/Api.dart
+++ youtube/lib/Api.dart
@@ -1,25 +1,33 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:youtube/model/Video.dart';
+
 const CHAVE_YOUTUBE_API = "AIzaSyAaEY1Rg0elo9nIbz4qsrUCgflfy3mC8Fc";
-const ID_CANAL = "edublinTV";
+const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
 const URL_BASE = "https://www.googleapis.com/youtube/v3";
 
 class Api {
   pesquisar(String pesquisa) async {
-    String url = URL_BASE + "/search";
-    // "?part=snippet" +
-    // "&type=video" +
-    // "&maxResults=20" +
-    // "&order=date" +
-    // "&key=${CHAVE_YOUTUBE_API}" +
-    // "&channelId=${ID_CANAL}";
-    // "&q=${pesquisa}"
-    print(url);
+    String url = URL_BASE +
+        "/search" +
+        "?key=$CHAVE_YOUTUBE_API" +
+        "&type=video" +
+        "&maxResults=20" +
+        "&order=date" +
+        "&q=$pesquisa" +
+        "&channelId=$ID_CANAL" +
+        "&part=snippet";
 
-    http.Response response = await http.get(
-      Uri.parse(url),
-    );
-    print(response.body);
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dadosJson = json.decode(response.body);
+      List<Video> videos = dadosJson["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+      for (var video in videos) {
+        print(video.titulo);
+      }
+    } else {}
   }
 }
