acdc9e677918f32c8f97d4b55d2d29c10f99f282
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-02-07 22:38:06 -0300

    finished app example

--- shared_preferences_app/lib/Home.dart
+++ shared_preferences_app/lib/Home.dart
@@ -12,13 +12,37 @@ class _HomeState extends State<Home> {
   TextEditingController _controllerCampo = TextEditingController();
   String _textoSalvo = "Nada salvo!";
 
-  void _salvar() {
+  void _salvar() async {
     String valorDigitado = _controllerCampo.text;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("nome", valorDigitado);
+    _controllerCampo.text = "";
+    setState(() {
+      _textoSalvo = "";
+    });
   }
 
-  void _recuperar() {}
+  void _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? valorDigitado = await prefs.getString("nome");
+    setState(() {
+      if (valorDigitado == null) {
+        _textoSalvo = "Nada consta!";
+      } else {
+        _textoSalvo = valorDigitado;
+      }
+    });
+  }
 
-  void _remover() {}
+  void _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isRemoved = await prefs.remove("nome");
+    if (isRemoved) {
+      setState(() {
+        _textoSalvo = "";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +54,6 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.all(32),
         child: Column(
           children: [
-            Text(
-              _textoSalvo,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
             TextField(
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
@@ -74,7 +92,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   RaisedButton(
-                    onPressed: _recuperar,
+                    onPressed: _remover,
                     color: Colors.blue,
                     textColor: Colors.white,
                     padding: EdgeInsets.all(15),
@@ -86,6 +104,17 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 30,
+              ),
+              child: Text(
+                _textoSalvo,
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
             ),
           ],
