386465db453305d6df75a48ebb4a5dba3f3369b2
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-03-13 15:48:07 -0300

    add new rules to use Flutter web

--- flutter_web_project/lib/main.dart
+++ flutter_web_project/lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_web_project/responsividade_media_query.dart';
-import 'package:flutter_web_project/responsividade_row_col.dart';
+import 'package:flutter_web_project/orientacao.dart';
+import 'package:flutter_web_project/regras_layout.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Flutter Web",
     debugShowCheckedModeBanner: false,
-    // home: ResposividadeMediaQuery(),
-    home: ResponsividadeRowCol(),
+    home: RegrasLayout(),
   ));
 }
