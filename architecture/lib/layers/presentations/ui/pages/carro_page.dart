cf2b48a06ae7ceace4ec148d6acf4a5c16295c2e
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-04-23 18:14:47 -0300

    removido imports nao usados

--- architecture/lib/layers/presentations/ui/pages/carro_page.dart
+++ architecture/lib/layers/presentations/ui/pages/carro_page.dart
@@ -1,8 +1,3 @@
-import 'package:architecture/layers/data/datasources/local/get_carros_por_cor_local_datasource_imp.dart';
-import 'package:architecture/layers/data/repositories/get_carro_por_cor_repository_imp.dart';
-import 'package:architecture/layers/data/repositories/salvar_carro_favorito_repository_imp.dart';
-import 'package:architecture/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_imp.dart';
-import 'package:architecture/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_imp.dart';
 import 'package:architecture/layers/presentations/controllers/carro_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
